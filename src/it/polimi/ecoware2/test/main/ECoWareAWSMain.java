d228fdbf91a103603772928738ae0d9589be6680
Giovanni Quattrocchi <giovanni.quattrocchi@beqid.com>
2016-02-20 16:08:33 +0100

    fix

--- src/it/polimi/ecoware2/test/main/ECoWareAWSMain.java
+++ src/it/polimi/ecoware2/test/main/ECoWareAWSMain.java
@@ -68,6 +68,7 @@ public class ECoWareAWSMain
 			end = true; }));
 		
 		probe = new AWSProbe(new Allocation(2*(long)1E9, 1), awsAccessKey, awsSecretKey, Regions.US_WEST_2, "control-aws-experiments");
+		Bus.getShared().put(Commons.CURRENT_ALLOCATION_KEY, probe.getCurrentAllocation());
 
 		while(probe.getCurrentAllocation().getC() != initialAlloc.getC()){
 			System.out.println(probe.getCurrentAllocation());
