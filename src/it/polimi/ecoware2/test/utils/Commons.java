6bf69cb6813769d60b4d6c70730a6669100a0e30
Giovanni Quattrocchi <giovanni.quattrocchi@beqid.com>
2016-03-01 18:48:44 +0100

    pwitter test

--- src/it/polimi/ecoware2/test/utils/Commons.java
+++ src/it/polimi/ecoware2/test/utils/Commons.java
@@ -15,7 +15,7 @@ public class Commons
 
 	public static final String CONTAINER_HOST = "131.175.135.184";
 	public static final int CONTAINER_PORT = 2375;
-	public static final String SERVER_HOST = "rubis-lb-1361474174.us-west-2.elb.amazonaws.com";
+	public static final String SERVER_HOST = "pwitter-lb-1145964028.us-west-2.elb.amazonaws.com";
 	public static final int SERVER_PORT = 80;
 	public static final String CONTAINER_ID = "0646d0cd73155d57cff79533dfb662d32c06ed881eec898bf8e01198b6a0ce76";
 	
@@ -26,5 +26,6 @@ public class Commons
 	public static final int SAMPLE_TIME = 10;
 
 	public static final String EXECUTOR_EXECUTE_ENDPOINT = "http://localhost:8000/api/executor";
+	public static final String SERVER_PATH = "pweets"; //"/rubis/servlet/BrowseRegions";
 
 }
